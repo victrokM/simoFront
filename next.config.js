e64734d851b634f965dac0e5a75a89acc76d7619
Victor Monsalve <victrokwtf@gmail.com>
2023-06-23 01:31:26 -0500

    :hammer: adding init configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: []
+  },
+  swcMinify: true,
+  webpack: (config, options) => {
+    const { dir, defaultLoaders } = options;
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      include: [dir],
+      exclude: /node_modules/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: 'ts-loader',
+          options: { compilerOptions: { noEmit: false } }
+        }
+      ]
+    });
 
-module.exports = nextConfig
+    return config;
+  }
+};
+
+module.exports = nextConfig;
